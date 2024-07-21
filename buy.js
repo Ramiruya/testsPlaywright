ed19f695c612b6eff341309a6226da757676b0b9
Ram <43316723+Ramiruya@users.noreply.github.com>
2024-07-22 01:06:30 +0300

    проверка добавления товара в корзину
    Происходит проверка авторизации и перехода на страницу товаров, далее проверяется корректное добавление товара в корзину, переход на страницу корзины и правильное количество товаров.

---
+++ buy.js
@@ -0,0 +1,34 @@
+const { chromium } = require('playwright');
+const { expect } = require('@playwright/test');
+
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  try {
+
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+    const cartBadge = await page.locator('.shopping_cart_badge').textContent();
+    expect(cartBadge).toBe('2');
+    await page.locator('.shopping_cart_link').click();
+
+    await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
+    const cartItem = await page.locator('.cart_item').count();
+    expect(cartItem).toBe(2);
+    console.log('Sucsess!')
+  } catch (error) {
+    console.error('Test failed:', error);
+  } finally {
+    await browser.close();
+  }
+})();
